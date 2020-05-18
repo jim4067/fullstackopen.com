919278f018f08482946bd6cd9d41ab6120e00b03
jim4067 <jimmyimpulse2@gmail.com>
2020-05-18 21:16:41 +0300

    finished 1.7: unicafe step2 with average working as it should and a lot of bloat code

--- part 1/unicafe/unicafe/src/index.js
+++ part 1/unicafe/unicafe/src/index.js
@@ -6,31 +6,47 @@ const App = () => {
   const [good , setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad , setBad] = useState(0);
-
-  const increaseGood = () => {
-    setGood(good + 1 );
-  };
-  const increaseNeutral = () => {
-    setNeutral ( neutral + 1 );
-  };
-  const increaseBad = () => {
-    setBad ( bad + 1 );
-  };
   
-  console.log("good " , good, "neutral " , neutral, "bad ", bad);
+  //the points for calculating average
+  const [bad_point , setBad_point] = useState(0);
+  const [good_point , setGood_point] = useState(0);
+  const [neutral_point, setNeutral_point] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1 );
+    setGood_point(good_point + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral ( neutral + 1 );
+    setNeutral_point(neutral_point + 0);
+  };
+  const handleBad = () => {
+    setBad ( bad + 1 );
+    setBad_point (bad_point - 1 );
+  };
+
+  //do not have to type the addition again 
+  const all = good + neutral + bad;
+  
+  
+
+  console.log("good " , good, "bad ", bad,   "bad_point ", bad_point, "all" , all);
 
   return (
     <div>
       <h1>
         give feedback
       </h1>
-      <button onClick = {increaseGood}>good</button>
-      <button onClick = {increaseNeutral}>neutral</button>
-      <button onClick = {increaseBad} >bad</button>
+      <button onClick = {handleGood}>good</button>
+      <button onClick = {handleNeutral}>neutral</button>
+      <button onClick = {handleBad}  >bad</button>
       <h1>statistics</h1>
-      <Display text = "good"  stats = {good} />
-      <Display text = "neutral"  stats = {neutral} />
-      <Display text = "bad"  stats = {bad} />
+      <Display text = "Good"  stats = {good} />
+      <Display text = "Neutral"  stats = {neutral} />
+      <Display text = "Bad"  stats = {bad} />
+      <Display text = "All" stats = {all } />
+      <Display text = "Average" stats = {  (bad_point + good_point ) / all } />
+      <Display text = "Positive" stats = {(good/all)*100} />
       
     </div>
   );
