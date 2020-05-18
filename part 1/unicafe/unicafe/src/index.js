48c1188174202c14706ac53f3cf7dd83b658c6a3
jim4067 <jimmyimpulse2@gmail.com>
2020-05-18 14:08:32 +0300

    finished with 1.6: unicafe step1

--- part 1/unicafe/unicafe/src/index.js
+++ part 1/unicafe/unicafe/src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const [good , setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad , setBad] = useState(0);
+
+  const increaseGood = () => {
+    setGood(good + 1 );
+  };
+  const increaseNeutral = () => {
+    setNeutral ( neutral + 1 );
+  };
+  const increaseBad = () => {
+    setBad ( bad + 1 );
+  };
+  
+  console.log("good " , good, "neutral " , neutral, "bad ", bad);
+
+  return (
+    <div>
+      <h1>
+        give feedback
+      </h1>
+      <button onClick = {increaseGood}>good</button>
+      <button onClick = {increaseNeutral}>neutral</button>
+      <button onClick = {increaseBad} >bad</button>
+      <h1>statistics</h1>
+      <Display text = "good"  stats = {good} />
+      <Display text = "neutral"  stats = {neutral} />
+      <Display text = "bad"  stats = {bad} />
+      
+    </div>
+  );
+};
+
+const Display = (props) => {
+
+  return (
+    <div>
+      {props.text} {props.stats}
+    </div>
+  );
+};
+
+
+ReactDOM.render(<App /> ,document.getElementById ('root') );
