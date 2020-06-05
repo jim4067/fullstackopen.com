5b7637b1224355af27d7d9986f693b030d828489
jim4067 <jimmyimpulse2@gmail.com>
2020-06-05 23:14:44 +0300

    finished 2.17: Phonebook step9 && 2.18*: Phonebook step10

--- part 2/thephonebook/src/App.js
+++ part 2/thephonebook/src/App.js
@@ -28,10 +28,13 @@ const App = () => {
       name: newName,
       number: newNum
     };
-    axios.post('http://localhost:3001/persons', personObect)
-    setPersons(persons.concat(personObect));
-    setNewName("");
-    setNewNum("");
+    PersonService
+            .create(personObect)
+            .then( response => {
+              setPersons(persons.concat(response) )
+              setNewName("");
+              setNewNum("");
+            } )
   };
 
 
@@ -46,8 +49,32 @@ const App = () => {
     setNewNum(event.target.value);
   }
 
+  /*the function that alerts when entering dulicate values
   function handleRepeat() {
     persons.filter((nameparam) => newName === nameparam.name ? alert(`${newName} already exist`) : newName);
+  }
+  */
+ function handleRepeat (id) {
+   const url = `http://localhost:3001/persons/${id}`;
+   const person = persons.find( personparam => personparam.id === id);
+   const changedPerson = {...persons}
+
+   console.log('what the hell does the console.log return', person)
+   persons.filter((nameparam) => newName === nameparam.name ? alert(`${newName} already exist,replace it`) : newName);
+   axios.put(url, changedPerson).then(response => {
+    setPersons(persons.map(personParam => personParam.id !== id ? person : response.data))
+  })
+ }
+
+  //the function that will be used for deleting users
+  const handleDelOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const person = persons.find( p => p.id === id)
+    const changedNote = { ...person }
+  
+    axios.delete(url, changedNote).then(response => {
+      setPersons(persons.map(p => p.id !== id ? p : response.data))
+    })
   }
 
 
@@ -58,10 +85,10 @@ const App = () => {
       <Filter value={newName} persons={persons} />
 
       <Personform addPerson={addPerson} newname={newName} handlePersonadd={handlePersonadd} newNum={newNum}
-        handleNumadd={handleNumadd} handleRepeat={handleRepeat} />
+        handleNumadd={handleNumadd} handleRepeat={handleRepeat(persons.id)} />
 
       <h3> Numbers </h3>
-      <Display persons={persons} />
+      <Display persons={persons} handleDel={ () => handleDelOf(persons.id)} />
 
     </div>
   );
--- part 2/thephonebook/src/components/Display.js
+++ part 2/thephonebook/src/components/Display.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const Display = (props) => {
-    
+    const label = props.persons.name === "" ? alert("deleted") : "Delete";
+
     return (
         <div>
-                {props.persons.map((personParam, i) =>
-                    <p key={i}>  {personParam.name} {personParam.number} </p>
-                )}
+            {props.persons.map((personParam, i) =>
+                <p key={i}>
+                    {personParam.name} {personParam.number}
+                    <button onClick={props.handleDel}> {label} </button>
+                </p>
+            )}
         </div>
     );
 }
